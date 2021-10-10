f38d30c1dbd1ccdcebd05021666b0d9128f9bc52
KimYongKwon <dydrnjs518@gmail.com>
2021-10-10 18:55:45 +0900

    feat: car 이름을 인자로한 객체 생성, car 거리누적 테스트 및 구현

--- src/main/java/racinggame/Car.java
+++ src/main/java/racinggame/Car.java
@@ -5,10 +5,25 @@ public class Car {
     public static final int CAR_NAME_LIMIT_LENGTH = 5;
 
     private int distance = 0;
+    private String name = null;
+
+    public Car(String inputName) {
+        if (!validCarName(inputName))
+            throw new IllegalArgumentException("자동차 이름 길이가 5 넘음");
+        this.name = inputName;
+    }
 
     public static boolean validCarName(String carName) {
         if (carName.length() <= CAR_NAME_LIMIT_LENGTH)
             return true;
         return false;
+    }
+
+    public void moveDistance(int moveDistance) {
+        this.distance += moveDistance;
+    }
+
+    public int getMoveDistance() {
+        return this.distance;
     }
 }
---
+++ src/test/java/racinggame/CarTest.java
@@ -0,0 +1,46 @@
+package racinggame;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CarTest {
+
+    @Test
+    @DisplayName("자동차는 만들어질때 각자의 이름을 갖는다. 이름이 5가 넘어가면 예외 발생")
+    public void CarConstruct() {
+
+        assertDoesNotThrow(() -> {
+            new Car("yong");
+        });
+
+        assertDoesNotThrow(() -> {
+            new Car("hi");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Car("yongkwon");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Car("hellobro");
+        });
+    }
+
+    @Test
+    @DisplayName("자동차는 움직일때마다 움직인 거리는 누적된다.")
+    public void CarMoveDistanceTest() {
+        Car car = new Car("test");
+        car.moveDistance(1);
+        Assertions.assertThat(car.getMoveDistance()).isEqualTo(1);
+
+        car.moveDistance(5);
+        Assertions.assertThat(car.getMoveDistance()).isEqualTo(6);
+
+        car.moveDistance(4);
+        Assertions.assertThat(car.getMoveDistance()).isEqualTo(10);
+    }
+}
